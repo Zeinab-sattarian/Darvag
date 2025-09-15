c6490fa9947f9498f3c955834ab4350a69eb4cd9
Zaynab <sattarianzeinab@gmail.com>
2025-09-15 18:52:56 +0330

    Stop tracking appsettings.Development.json

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d5566a06fe09fe5c1969bccc04075b7d6b15414")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91c197ce6af0217d06ca11242a79b75667de9eba")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
